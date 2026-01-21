094b8b8e094c3d1c6ec59ef0db5c30142fe131df
neo84716 <54547888+neo84716@users.noreply.github.com>
2026-01-21 22:10:52 +0800

    Cart頁面跟Bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './assets/scss/all.scss'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter basename="/sweetbox-react">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
