721a0f1ace62457f1f1a5ab555d4da98502265cf
yuan6636 <aleniverson777@gmail.com>
2026-01-26 18:12:09 +0800

    feat: 新增首頁

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
 import './assets/scss/all.scss'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
